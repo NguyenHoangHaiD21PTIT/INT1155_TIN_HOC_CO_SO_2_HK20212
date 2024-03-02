55898f3406bc1de59d9398c7afeb9e2d65f04fd7
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-03-02 09:39:29 +0700

    Update C07012 - SAP XEP MAT HANG.cpp

--- CAU TRUC/C07012 - SAP XEP MAT HANG.cpp
+++ CAU TRUC/C07012 - SAP XEP MAT HANG.cpp
@@ -17,20 +17,20 @@ int main(){
 	mh a[n];
 	for(int i=0;i<n;i++){
 		scanf ("\n");
-	    gets (a[i].ten);
-	    scanf("\n");
-	    gets(a[i].nhomhang);
-	    scanf ("%f%f", &a[i].giamua ,&a[i].giaban);
-	    a[i].loinhuan = a[i].giaban-a[i].giamua;
-	    a[i].stt = i+1;
+	        gets (a[i].ten);
+	        scanf("\n");
+	        gets(a[i].nhomhang);
+	        scanf ("%f%f", &a[i].giamua ,&a[i].giaban);
+	        a[i].loinhuan = a[i].giaban-a[i].giamua;
+	        a[i].stt = i+1;
 	}
 	for(int i=0;i<n;i++){
 		for(int j=i+1;j<n;j++){
 			if(a[i].loinhuan<a[j].loinhuan){
 				mh temp ;
-	            temp = a[i];
-	            a[i] = a[j] ;
-	            a[j] = temp;
+		                temp = a[i];
+		                a[i] = a[j];
+		                a[j] = temp;
 			}	
 		}
 		printf("%d %s %s %.2f\n", a[i].stt,a[i].ten,a[i].nhomhang,a[i].loinhuan);
