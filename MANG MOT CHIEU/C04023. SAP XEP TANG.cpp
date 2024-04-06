0466dba08b98cf5b5bea5826893f886339f420b1
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-04-06 17:08:19 +0700

    Update C04023. SAP XEP TANG.cpp

--- MANG MOT CHIEU/C04023. SAP XEP TANG.cpp
+++ MANG MOT CHIEU/C04023. SAP XEP TANG.cpp
@@ -9,13 +9,15 @@ int main(){
     int tg;
     for(int i = 0; i < n - 1; i++){
         for(int j = i + 1; j < n; j++){
-            if(a[i] < a[j]){
+            if(a[i] > a[j]){
                 tg = a[i];
                 a[i] = a[j];
                 a[j] = tg;        
             }
         }
     }
-    printf("%d %d", a[0], a[1]);
+        for(int i=0;i<n;i++){
+		printf("%d ", a[i]);
+    }
 }
     	
